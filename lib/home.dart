ba4b8b31e9533f584b688198f0190534bda5a66e
Numa <numaams@gmail.com>
2024-02-10 21:32:36 -0300

    Set layoutbuilder for responsiveness

--- lib/home.dart
+++ lib/home.dart
@@ -7,16 +7,55 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Text(
-          title,
-          style: const TextStyle(color: Colors.black),
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          title: Text(
+            title,
+            style: const TextStyle(color: Colors.black),
+          ),
         ),
-      ),
-      body: const Center(),
-      // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            if (constraints.maxWidth > 900) {
+              return const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //Info
+                  Expanded(
+                    child: Center(
+                      child: Text('Info'),
+                    ),
+                  ),
+                  SizedBox(width: 25),
+                  //List
+                  Expanded(
+                    child: Center(
+                      child: Text('List'),
+                    ),
+                  ),
+                ],
+              );
+            } else {
+              return PageView(
+                children: const [
+                  //Info
+                  Expanded(
+                    child: Center(
+                      child: Text('Info'),
+                    ),
+                  ),
+                  //List
+                  Expanded(
+                    child: Center(
+                      child: Text('List'),
+                    ),
+                  ),
+                ],
+              );
+            }
+          },
+        ));
   }
 }
