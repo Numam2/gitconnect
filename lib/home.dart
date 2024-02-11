9c7cc344db03281e8607cb66f7e9a2b409252465
Numa <numaams@gmail.com>
2024-02-10 21:55:10 -0300

    Create PageView for screens under 900px

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:git_connect/commit_list.dart';
+import 'package:git_connect/project_info.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   final String title;
   const Home(this.title, {super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  PageController _pageController = PageController(initialPage: 0);
+  bool showCommitList = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +26,7 @@ class Home extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Theme.of(context).colorScheme.primary,
           title: Text(
-            title,
+            widget.title,
             style: const TextStyle(color: Colors.black),
           ),
         ),
@@ -23,33 +38,86 @@ class Home extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   //Info
-                  Expanded(
-                    child: Center(
-                      child: Text('Info'),
-                    ),
-                  ),
+                  Expanded(child: ProjectInfo()),
                   SizedBox(width: 25),
                   //List
-                  Expanded(
-                    child: Center(
-                      child: Text('List'),
-                    ),
-                  ),
+                  Expanded(child: CommitList()),
                 ],
               );
             } else {
-              return PageView(
-                children: const [
-                  //Info
-                  Expanded(
-                    child: Center(
-                      child: Text('Info'),
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: SizedBox(
+                      height: 75,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          //Info
+                          TextButton(
+                              style: TextButton.styleFrom(
+                                  side: BorderSide(
+                                      color: !showCommitList
+                                          ? Theme.of(context)
+                                              .colorScheme
+                                              .primary
+                                          : Colors.transparent)),
+                              onPressed: () => setState(() {
+                                    showCommitList = false;
+                                    _pageController.animateToPage(0,
+                                        duration:
+                                            const Duration(milliseconds: 250),
+                                        curve: Curves.ease);
+                                  }),
+                              child: Text('Project Info',
+                                  style: TextStyle(
+                                      color: !showCommitList
+                                          ? Theme.of(context)
+                                              .colorScheme
+                                              .primary
+                                          : Colors.grey))),
+                          const SizedBox(width: 10),
+                          //List
+                          TextButton(
+                              style: TextButton.styleFrom(
+                                  side: BorderSide(
+                                      color: showCommitList
+                                          ? Theme.of(context)
+                                              .colorScheme
+                                              .primary
+                                          : Colors.transparent)),
+                              onPressed: () => setState(() {
+                                    showCommitList = true;
+                                    _pageController.animateToPage(1,
+                                        duration:
+                                            const Duration(milliseconds: 250),
+                                        curve: Curves.ease);
+                                  }),
+                              child: Text('Commit List',
+                                  style: TextStyle(
+                                      color: showCommitList
+                                          ? Theme.of(context)
+                                              .colorScheme
+                                              .primary
+                                          : Colors.grey))),
+                        ],
+                      ),
                     ),
                   ),
-                  //List
+                  //PageView
                   Expanded(
-                    child: Center(
-                      child: Text('List'),
+                    child: PageView(
+                      controller: _pageController,
+                      children: const [
+                        //Info
+                        ProjectInfo(),
+                        //List
+                        CommitList()
+                      ],
                     ),
                   ),
                 ],
